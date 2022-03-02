89424e7fbf4e1a4d0c4ddd3e0e1aa429100c9fb6
SunYiH <498611343@qq.com>
2022-03-02 17:33:03 +0800

    临时提交

--- app/src/main/java/com/sunexample/demoforandroidxandkotlin/LottieAnimDemo/LottieDemoActivity.kt
+++ app/src/main/java/com/sunexample/demoforandroidxandkotlin/LottieAnimDemo/LottieDemoActivity.kt
@@ -1,5 +1,6 @@
 package com.sunexample.demoforandroidxandkotlin.LottieAnimDemo
 
+import android.animation.Animator
 import android.animation.ValueAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -21,13 +22,58 @@ class LottieDemoActivity : AppCompatActivity() {
         binding = ActivityLottieDemoBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-//        Glide.with(this).load("file:///android_asset/cpu/images/img_0.png").into(binding.testImg)
 
-        binding.viewCooler.setImageAssetsFolder("cpu/images/");
+//        binding.viewCooler.setImageAssetsFolder("cpu/images/");
+//        binding.viewCooler.setAnimation("cpu/data.json");
+
+//        binding.viewCooler.setImageAssetsFolder("battery/images/");
+//        binding.viewCooler.setAnimation("battery/data.json");
+//
+//        binding.viewCooler.setImageAssetsFolder("boost/images/");
+//        binding.viewCooler.setAnimation("boost/data.json");
+
+//        binding.viewCooler.setImageAssetsFolder("clean/images/");
+//        binding.viewCooler.setAnimation("clean/data.json");
+
+
+//        binding.viewCooler.setImageAssetsFolder("completed/images/");
+//        binding.viewCooler.setAnimation("completed/data.json");
+
+
+//        binding.viewCooler.setImageAssetsFolder("scan/images/");
+//        binding.viewCooler.setAnimation("scan/data.json");
+
+//        binding.viewCooler.setImageAssetsFolder("security/images/");
+//        binding.viewCooler.setAnimation("security/data.json");
+
+//        binding.viewCooler.setImageAssetsFolder("security_init/images/");
+//        binding.viewCooler.setAnimation("security_init/data.json");
+
+
+        binding.viewCooler.repeatCount = 2
         binding.viewCooler.playAnimation()
-        binding.viewCooler.addAnimatorUpdateListener {
-            Log.d(TAG, "viewCooler PLAY")
-        }
+
+//        binding.viewCooler.addAnimatorUpdateListener {
+//            Log.d(TAG, "viewCooler PLAY")
+//        }
+        binding.viewCooler.addAnimatorListener(object : Animator.AnimatorListener {
+            override fun onAnimationStart(animation: Animator?) {
+                Log.d(TAG, "onAnimationStart")
+            }
+
+            override fun onAnimationEnd(animation: Animator?) {
+                Log.d(TAG, "onAnimationEnd")
+            }
+
+            override fun onAnimationCancel(animation: Animator?) {
+                Log.d(TAG, "onAnimationCancel")
+            }
+
+            override fun onAnimationRepeat(animation: Animator?) {
+                Log.d(TAG, "onAnimationRepeat")
+            }
+
+        })
 
     }
 }
